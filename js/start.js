4037b42c6b24662b425e46f9310d5453eb58b5f1
Nelli <nellinosovapl@gmail.com>
2020-09-19 18:48:45 +0200

    Реализовала гистограмму

--- js/start.js
+++ js/start.js
@@ -3,18 +3,20 @@
 var COLD_WIDTH = 300;
 var COLD_HEIGHT = 250;
 
-var PLAYER_NAME_X = 130;
+var PLAYER_NAME_X = 125;
 var PLAYER_NAME_Y = 235;
 
 var PLAYER_BAR_X = 125;
 var PLAYER_BAR_Y = 70;
 var PLAYER_BAR_WIDTH = 40;
-var PLAYER_BAR_HEIGHT = 140;
+var PLAYER_BAR_HEIGHT = 150;
 
-var GAP = 60;
+var GAP = PLAYER_BAR_WIDTH + 50;
 
-var playerIndex = 0;
-var playerName = 'Вы';
+// var playerIndex = 0;
+// var playerName = 'Вы';
+
+
 
 
 var renderCould = function(ctx, x, y, color) {
@@ -25,55 +27,46 @@ var renderCould = function(ctx, x, y, color) {
   ctx.fillRect(x - 180, y + 60, COLD_WIDTH * 2, COLD_HEIGHT - 110);
 };
 
-window.renderStatistics = function(ctx) {
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
   renderCould(ctx, 230, 30, 'rgba(0, 0, 0, 0.3)', 300, 250);
   renderCould(ctx, 220, 20, '#fff', 300, 250);
 
-  ctx.fillStyle = '#000';
 
-  ctx.fillRect(
-    PLAYER_BAR_X + GAP*0,
-    PLAYER_BAR_Y,
-    PLAYER_BAR_WIDTH,
-    PLAYER_BAR_HEIGHT
-  );
-  ctx.fillText(
-    'Вы',
-    PLAYER_NAME_X + GAP*0,
-    PLAYER_NAME_Y
-  );
 
-  ctx.fillRect(
-    PLAYER_BAR_X + GAP*1,
-    PLAYER_BAR_Y,
-    PLAYER_BAR_WIDTH,
-    PLAYER_BAR_HEIGHT
-  );
-  ctx.fillText(
-    'Иван',
-    PLAYER_NAME_X + GAP*1,
-    PLAYER_NAME_Y
-  );
 
-  ctx.fillRect(
-    PLAYER_BAR_X + GAP*2,
-    PLAYER_BAR_Y ,
-    PLAYER_BAR_WIDTH,
-    PLAYER_BAR_HEIGHT
-  );
-  ctx.fillText(
-    'Юлия',
-    PLAYER_NAME_X + GAP*2,
-    PLAYER_NAME_Y
-  );
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+
+    var playerBarY = PLAYER_BAR_Y + PLAYER_BAR_HEIGHT - (PLAYER_BAR_HEIGHT * times[i]) / maxTime;
+
+    var playerBarHeight = (PLAYER_BAR_HEIGHT * times[i]) / maxTime;
+    ctx.fillStyle = '#000';
+    ctx.fillText(
+      Math.round(times[i]),
+      PLAYER_NAME_X + GAP * i,
+      playerBarY - 7
+    );
+    ctx.fillRect(
+      PLAYER_BAR_X + GAP * i,
+      playerBarY,
+      PLAYER_BAR_WIDTH,
+      playerBarHeight
+    );
+    ctx.fillText(
+      players[i],
+      PLAYER_NAME_X + GAP * i,
+      PLAYER_NAME_Y
+    );
+  }
 };
-
-// ctx.fillRect(125, 70, 40, 140);
-// ctx.fillText('Вы', 130, 235);
-
-// ctx.fillRect(185, 70, 40, 140);
-// ctx.fillText('Иван', 190, 235);
-
-// ctx.fillRect(245, 70, 40, 140);
-// ctx.fillText('Юлия', 250, 235);
-
