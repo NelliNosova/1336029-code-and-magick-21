a9ce3900570e5bcf5b7b93e18ee987f899113f03
Nelli <nellinosovapl@gmail.com>
2020-09-26 18:23:43 +0200

    Убрала повторяющиеся значения параметров мага

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,7 @@
 
 var setupBox = document.querySelector('.setup');
 setupBox.classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
 
 var PLAYER_NAMES = [
   'Иван',
@@ -45,34 +46,33 @@ var PLAYER_EYES_COLORS = [
 
 var PLAYER_NUMBER = 4;
 
-var getRandomInt = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+var getShuffle = function (array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 };
 
-var getRandomItem = function (array) {
-  var index = getRandomInt(0, array.length);
-  return array[index];
-};
-
+var shuflePlayerNames = getShuffle(PLAYER_NAMES);
+var shuflePlayerSecondNames = getShuffle(PLAYER_SECOND_NAMES);
+var shuflePlayerCoatColors = getShuffle(PLAYER_COAT_COLORS);
+var shuflePlayerEyesColor = getShuffle(PLAYER_EYES_COLORS);
 
 var PLAYERS = [];
 for (var i = 0; i < PLAYER_NUMBER; i++) {
   var PLAYER = {
-    name: getRandomItem(PLAYER_NAMES) + ' ' + getRandomItem(PLAYER_SECOND_NAMES),
-    coatColor: getRandomItem(PLAYER_COAT_COLORS),
-    eyesColor: getRandomItem(PLAYER_EYES_COLORS)
+    name: shuflePlayerNames[i] + ' ' + shuflePlayerSecondNames[i],
+    coatColor: shuflePlayerCoatColors[i],
+    eyesColor: shuflePlayerEyesColor[i]
   };
 
   PLAYERS.push(PLAYER);
 }
 
-
-document.querySelector('.setup-similar').classList.remove('hidden');
-
 var similarListElement = document.querySelector('.setup-similar-list');
-var similarPlayerTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var similarPlayerTemplate = document.querySelector('#similar-wizard-template')
+    .content.querySelector('.setup-similar-item');
 
 for (var j = 0; j < PLAYER_NUMBER; j++) {
   var playerElement = similarPlayerTemplate.cloneNode(true);
